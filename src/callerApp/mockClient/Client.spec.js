4b88a41273b48d34b722f350715767ac97f204a2
SCP13 <sergi.campos2@sky.uk>
2024-07-12 08:42:14 +0100

    SCP13 | remove unnecessary code and add comments

--- src/callerApp/mockClient/Client.spec.js
+++ src/callerApp/mockClient/Client.spec.js
@@ -5,17 +5,19 @@ import Client from './Client.jsx';
 import {serviceProcess} from "../mockService/mockServiceController";
 // import useService from "./useService";
 
+jest.mock("../mockService/mockServiceController")
+
 const useService = jest.requireActual("./useService");
 const useServiceSpy = jest.spyOn(useService, "default");
-jest.mock("../mockService/mockServiceController")
 
 const setShowLoading = jest.fn();
 const setData = jest.fn();
 
+
 const renderComponent = () => render(<Client setShowLoading={setShowLoading} setData={setData}/>);
 
-
 describe('ClientManagement', () => {
+    
     beforeEach(() => {
         serviceProcess.mockReturnValue("Success");
     })
@@ -24,13 +26,18 @@ describe('ClientManagement', () => {
     })
 
     it('should render components ClientForm and ClientList', async() => {
+        // Given -> Preconditions to my test if any (none in this case)
+        // When -> Action of the test (the render in this case)
         renderComponent();
+        // Then --> Assertions
         screen.debug(undefined, 300000);
         const button = screen.getByRole('button', {name: "Click me to call the service"});
         expect(button).toBeInTheDocument();
         expect(button).toBeVisible();
         expect(useServiceSpy).toHaveBeenCalledWith(setShowLoading, setData)
+        // When -> Sometimes you'll find another interaction in the middle of the test
         await userEvent.click(button);
+        // Then -> Followed by some additional assertions
         expect(setData).toHaveBeenCalledWith('Success')
     });
 });
